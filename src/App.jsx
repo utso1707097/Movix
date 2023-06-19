07f93a57ed173d007f782a34e184c1964cb2ccf3
utso1707097 <utsomollik1707097@gmail.com>
2023-06-20 02:20:45 +0600

    minimal router setup,pages and components done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import { getApiConfiguration } from "./store/homeSlice";
 import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+
 function App() {
   const dispatch = useDispatch();
   const { url } = useSelector((state) => state.home);
@@ -17,7 +27,19 @@ function App() {
     });
   };
 
-  return <div>{url?.total_pages}</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id " element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
