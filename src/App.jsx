ee3c7d807e94e01e53ccec7e82160a8bd46feff3
utso1707097 <utsomollik1707097@gmail.com>
2023-06-25 02:24:36 +0600

    movie details page done

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/:mediaType/:id " element={<Details />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
         <Route path="/search/:query" element={<SearchResult />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<PageNotFound />} />
